221f2f4bdbd400e92fab8c43fffb687032c9e5b3
hoangeiu <leminhhoangk3@gmail.com>
2024-10-08 22:23:17 +0700

    first commit

---
+++ users/schema/schema.js
@@ -0,0 +1,44 @@
+const graphql = require("graphql");
+const axios = require("axios");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const DB = "http://localhost:3000";
+
+const users = [
+  {
+    id: "23",
+    firstName: "Hoang",
+    age: 29,
+  },
+  {
+    id: "47",
+    firstName: "Le",
+    age: 18,
+  },
+];
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`${DB}/users/${args.id}`).then((res) => res.data);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
